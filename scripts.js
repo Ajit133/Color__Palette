3e80891a4630d174634f3bc302378566a1bb45bb
Ajit133 <dasajit697@gmail.com>
2024-01-29 15:03:19 +0600

    create input button and show the hex color code

--- scripts.js
+++ scripts.js
@@ -5,16 +5,25 @@ window.onload = ()=>{
 function main(){
     const root = document.getElementById('root');
     const btn = document.getElementById('change-btn');
-
+    const inputBtn = document.getElementById("input_btn");
+   
     btn.addEventListener("click",()=>{
-       root.style.background = generateRGBcolor()
+       root.style.background = generateHEXcolor();
+       inputBtn.value = generateHEXcolor();
     })
 }
 
-function generateRGBcolor(){
+// function generateRGBcolor(){
+//     const red = Math.floor(Math.random()*255);
+//     const green = Math.floor(Math.random()*255);
+//     const blue = Math.floor(Math.random()*255);
+
+//     return `rgb(${red},${green},${blue})`;
+// }
+function generateHEXcolor(){
     const red = Math.floor(Math.random()*255);
     const green = Math.floor(Math.random()*255);
     const blue = Math.floor(Math.random()*255);
 
-    return `rgb(${red},${green},${blue})`;
+    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 }
